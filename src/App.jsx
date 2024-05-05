a5755d43049639ce7d1c940b9b0db0a12a2d0b86
prakhar3062 <prakharm.bt.20@nitj.ac.in>
2024-05-05 09:08:23 +0530

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,23 @@ import { Footer } from "./components/footer/Footer";
 import Body from "./components/Body/body";
 import { Address } from "./components/address/Address";
 import Venue from "./components/venue/Venue";
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <Header/>
-      <Venue/>
+      
       <Navbar/>
       {/* <Brocher/> */}
-      <Body/>
-      <Address/>
+      <Routes>
+          <Route path="/" element={<Venue />} />
+          <Route path="/body" element={<Body />} />
+      </Routes>
+          <Address/>
       <Footer/>
     </div>
+    </BrowserRouter>
   );
 }
 
